97a616f2f1e194332675898dcba038827cae23f9
Michal Parusinski <michal@parusinski.me>
2022-03-24 15:11:41 +0100

    added support for transparency

--- config.h
+++ config.h
@@ -93,6 +93,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
@@ -507,4 +510,5 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+        { "alpha",        FLOAT,   &alpha },
 };
