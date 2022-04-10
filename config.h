7c9d93e4887874469a39a868b7a17e20d36dd5c8
Michal Parusinski <michal@parusinski.me>
2022-04-10 14:27:54 +0200

    support for font 2

--- config.h
+++ config.h
@@ -6,6 +6,9 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { 
+    "JoyPixels:pixelsize=14:antialias=true:autohint=true",
+};
 static int borderpx = 2;
 
 /*
@@ -481,6 +484,7 @@ static char ascii_printable[] =
  */
 ResourcePref resources[] = {
 		{ "font",         STRING,  &font },
+		{ "font2",        STRING,  &font2[0] },
 		{ "color0",       STRING,  &colorname[0] },
 		{ "color1",       STRING,  &colorname[1] },
 		{ "color2",       STRING,  &colorname[2] },
